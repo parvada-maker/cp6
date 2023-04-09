7e5760b0b25d73e0e0bedebc2c5dc1553339906f
isaacjosephnew <120455859+isaacjosephnew@users.noreply.github.com>
2023-04-09 20:29:40 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	var ball_options={
+
+		isStatic:false,
+		restituition:0.3
+	}
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+ball=Bodies.circle(100,200,30,ball_options)
+World.add(ball,world)
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
